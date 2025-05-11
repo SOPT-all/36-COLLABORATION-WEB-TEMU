614beccdb3554794af3a2e5ed03601381f0bf615
1jiwoo27 <chloey1227@naver.com>
2025-05-11 20:08:51 +0900

    [fix] svg 컴포넌트 import 오류 해결

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,8 @@
 /// <reference types="vite/client" />
-declare module '*.svg' {
-    import * as React from 'react';
-    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
-    const src: string;
-    export default src;
-  }
+/// <reference types="vite-plugin-svgr/client" />
+declare module '*.svg?react' {
+  import * as React from 'react';
+  export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>;
+  const src: string;
+  export default src;
+}
