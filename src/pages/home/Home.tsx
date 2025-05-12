18353d1177263d68521199dcf54d29d4a82abd0f
hamxxn <hamin031121@gmail.com>
2025-05-12 15:45:32 +0900

    [feature] home page 필요없는 코드 제거

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,27 +1,5 @@
-import Card from '@shared/components/card/Card';
-
 const Home = () => {
-  const dummyCardData = {
-    size: 'l',
-    img: 'https://placehold.co/600x400',
-    repurchase: true,
-    title: 'USM Haller Storage Unit',
-    discount: 15,
-    price: 89000,
-  };
-
-  return (
-    <div style={{ padding: '2rem' }}>
-      <Card
-        size={dummyCardData.size}
-        repurchase={dummyCardData.repurchase}
-        img={dummyCardData.img}
-        title={dummyCardData.title}
-        discount={dummyCardData.discount}
-        price={dummyCardData.price}
-      />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
