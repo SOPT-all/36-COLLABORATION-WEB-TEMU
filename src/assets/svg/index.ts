e454c8d0f20ff1e32f265545f2c6b1bb9c0a5397
heesunee <heesun729@uos.ac.kr>
2025-05-12 17:52:02 +0900

    [feat] svg 파일 index.ts 작성

---
+++ src/assets/svg/index.ts
@@ -0,0 +1,79 @@
+// Arrow icons
+export { default as IcArrowDownGrayS } from '@svg/ic_arrow_down_gray_s.svg?react';
+export { default as IcArrowDownGray } from '@svg/ic_arrow_down_gray.svg?react';
+export { default as IcArrowDownWhite } from '@svg/ic_arrow_down_white.svg?react';
+export { default as IcArrowLineGray } from '@svg/ic_arrow_line_gray.svg?react';
+export { default as IcArrowRightBlack } from '@svg/ic_arrow_right_black.svg?react';
+export { default as IcArrowRightGray } from '@svg/ic_arrow_right_gray.svg?react';
+export { default as IcArrowRightGreen } from '@svg/ic_arrow_right_green.svg?react';
+
+// Baby and Book icons
+export { default as IcBabyBlack } from '@svg/ic_baby_black.svg?react';
+export { default as IcBabyWhite } from '@svg/ic_baby_white.svg?react';
+export { default as IcBookBlack } from '@svg/ic_book_black.svg?react';
+export { default as IcBookWhite } from '@svg/ic_book_white.svg?react';
+
+// Car and Cart icons
+export { default as IcCarBlack } from '@svg/ic_car_black.svg?react';
+export { default as IcCarWhite } from '@svg/ic_car_white.svg?react';
+export { default as IcCartBlack } from '@svg/ic_cart_black.svg?react';
+export { default as IcCartGray7 } from '@svg/ic_cart_gray7.svg?react';
+export { default as IcCart } from '@svg/ic_cart.svg?react';
+export { default as IcCategory } from '@svg/ic_category.svg?react';
+export { default as IcCheckGreen } from '@svg/ic_check_green.svg?react';
+export { default as IcChevronForwardBlack } from '@svg/ic_chevron_forward_black.svg?react';
+export { default as IcClose } from '@svg/ic_close.svg?react';
+
+// Digital icons
+export { default as IcDigitalBlack } from '@svg/ic_digital_black.svg?react';
+export { default as IcDigitalWhite } from '@svg/ic_digital_white.svg?react';
+export { default as IcFlashBlack } from '@svg/ic_flash_black.svg?react';
+export { default as IcFlashOrange } from '@svg/ic_flash_orange.svg?react';
+
+// Goods icons
+export { default as IcGoodsBlack } from '@svg/ic_goods_black.svg?react';
+export { default as IcGoodsWhite } from '@svg/ic_goods_white.svg?react';
+
+// Kitchen icons
+export { default as IcKitchenBlack } from '@svg/ic_kitchen_black.svg?react';
+export { default as IcKitchenWhite } from '@svg/ic_kitchen_white.svg?react';
+export { default as IcLanguage } from '@svg/ic_language.svg?react';
+
+// Necessity icons
+export { default as IcNecessityBlack } from '@svg/ic_necessity_black.svg?react';
+export { default as IcNecessityWhite } from '@svg/ic_necessity_white.svg?react';
+
+// Page navigation icons
+export { default as IcPageLeftGray } from '@svg/ic_page_left_gray.svg?react';
+export { default as IcPageRightGray } from '@svg/ic_page_right_gray.svg?react';
+export { default as IcPlantGreen } from '@svg/ic_plant_green.svg?react';
+
+// Recommendation icons
+export { default as IcRecommendBlack } from '@svg/ic_recommend_black.svg?react';
+export { default as IcRecommendWhite } from '@svg/ic_recommend_white.svg?react';
+export { default as IcReturnGreen } from '@svg/ic_return_green.svg?react';
+
+// Review icons
+export { default as IcReviewBlack } from '@svg/ic_review_black.svg?react';
+export { default as IcReviewFilter } from '@svg/ic_review_filter.svg?react';
+export { default as IcReviewWhite } from '@svg/ic_review_white.svg?react';
+export { default as IcShare } from '@svg/ic_share.svg?react';
+export { default as IcShieldGreen } from '@svg/ic_shield_green.svg?react';
+
+// Sport icons
+export { default as IcSportBlack } from '@svg/ic_sport_black.svg?react';
+export { default as IcSportWhite } from '@svg/ic_sport_white.svg?react';
+
+// Star icons
+export { default as IcStar2Sm } from '@svg/ic_star_2_sm.svg?react';
+export { default as IcStar3Sm } from '@svg/ic_star_3_sm.svg?react';
+export { default as IcStar45Sm } from '@svg/ic_star_4_5_sm.svg?react';
+export { default as IcStar45 } from '@svg/ic_star_4_5.svg?react';
+export { default as IcStar4Sm } from '@svg/ic_star_4_sm.svg?react';
+export { default as IcStar4 } from '@svg/ic_star_4.svg?react';
+export { default as IcStar5 } from '@svg/ic_star_5.svg?react';
+export { default as IcStar5Sm } from '@svg/ic_star_5_sm.svg?react';
+
+// Miscellaneous icons
+export { default as IcTruckGreen } from '@svg/ic_truck_green.svg?react';
+export { default as IcUser } from '@svg/ic_user.svg?react';
