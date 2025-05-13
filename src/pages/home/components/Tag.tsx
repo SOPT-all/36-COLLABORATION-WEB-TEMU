f8071558e002f7e8414966c88981685fd346477d
hamxxn <hamin031121@gmail.com>
2025-05-13 09:57:53 +0900

    [code review] 중복 svg 삭제

--- src/pages/home/components/Tag.tsx
+++ src/pages/home/components/Tag.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react';
 import * as styles from './Tag.css.ts';
 
-import Ic_heart from '@svg/ic_tag_heart.svg?react';
-import Ic_book from '@svg/ic_tag_book.svg?react';
-import Ic_car from '@svg/ic_tag_car.svg?react';
-import Ic_clothes from '@svg/ic_tag_clothes.svg?react';
-import Ic_digital from '@svg/ic_tag_digital.svg?react';
-import Ic_house from '@svg/ic_tag_house.svg?react';
-import Ic_infant from '@svg/ic_tag_infant.svg?react';
-import Ic_kitchen from '@svg/ic_tag_kitchen.svg?react';
-import Ic_sport from '@svg/ic_tag_sport.svg?react';
+import Ic_heart from '@svg/ic_recommend_black.svg?react';
+import Ic_book from '@svg/ic_book_black.svg?react';
+import Ic_car from '@svg/ic_car_black.svg?react';
+import Ic_clothes from '@svg/ic_goods_black.svg?react';
+import Ic_digital from '@svg/ic_digital_black.svg?react';
+import Ic_house from '@svg/ic_necessity_black.svg?react';
+import Ic_infant from '@svg/ic_baby_black.svg?react';
+import Ic_kitchen from '@svg/ic_kitchen_black.svg?react';
+import Ic_sport from '@svg/ic_sport_black.svg?react';
 
 const Tag = () => {
   const [selectedTag, setSelectedTag] = useState<string | null>(null);
