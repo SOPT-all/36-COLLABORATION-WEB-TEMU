0bb04ceebab8ecbfb2316f436a2159a4dc524119
jeonghoon11 <jih361@naver.com>
2025-05-15 23:12:25 +0900

    [feat] Review 데이터 임시 추가(#50)

---
+++ src/pages/productDetail/productReviewSection/reviewData.ts
@@ -0,0 +1,49 @@
+export const data = {
+  avgScore: 4.5,
+  reviewImages: ['image', 'image'],
+  productReviewDetails: [
+    {
+      nickname: 'dlwjddus1112',
+      score: 5,
+      imageUrl: 'image',
+      content: '너무 맛있어요',
+      productOption: '색상_베이지',
+      createdAt: '2025-05-11',
+    },
+    {
+      nickname: 'dlwjddus123',
+      score: 4,
+      imageUrl: 'image',
+      content: '가격이 창렬이네요',
+      productOption: '색상_카키',
+      createdAt: '2025-05-12',
+    },
+  ],
+  reviewScoreDistributions: [
+    {
+      score: 5,
+      reviewCount: 1,
+      percentage: 50,
+    },
+    {
+      score: 4,
+      reviewCount: 1,
+      percentage: 50,
+    },
+    {
+      score: 3,
+      reviewCount: 0,
+      percentage: 0,
+    },
+    {
+      score: 2,
+      reviewCount: 0,
+      percentage: 0,
+    },
+    {
+      score: 1,
+      reviewCount: 0,
+      percentage: 0,
+    },
+  ],
+};
