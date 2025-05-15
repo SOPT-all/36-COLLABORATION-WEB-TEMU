8adaecda31f881e60f47ffb46ed8b07caa06406b
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-05-15 18:10:25 +0900

    [code review] tag 상수 constant 폴더로 분리 및 id 추가, text 변경(#36)

--- src/pages/home/components/Tag.tsx
+++ src/pages/home/components/Tag.tsx
@@ -1,47 +1,23 @@
 import { useState } from 'react';
 import * as styles from '@/pages/home/components/Tag.css';
-
-import {
-  IcRecommendBlack,
-  IcBookBlack,
-  IcCarBlack,
-  IcGoodsBlack,
-  IcDigitalBlack,
-  IcNecessityBlack,
-  IcBabyBlack,
-  IcKitchenBlack,
-  IcSportBlack,
-} from '@svg/index.ts';
 import Text from '@shared/components/text/Text';
+import { TAG } from './constant/tags';
 
 const Tag = () => {
   const [selectedTag, setSelectedTag] = useState<string | null>(null);
-
-  const TAG = [
-    { text: '인기', icon: <IcRecommendBlack width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '도서', icon: <IcBookBlack width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '자동차용품', icon: <IcCarBlack width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '패션의류', icon: <IcGoodsBlack width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '디지털기기', icon: <IcDigitalBlack width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '생활/주거', icon: <IcNecessityBlack width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '유아동', icon: <IcBabyBlack width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '주방용품', icon: <IcKitchenBlack width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '스포츠', icon: <IcSportBlack width={'3.2rem'} height={'3.2rem'} /> },
-  ];
 
   return (
     <div className={styles.tagWrapper}>
       {TAG.map(tag => (
         <div
           className={styles.tag({ selected: selectedTag === tag.text })}
-          key={tag.text}
+          key={tag.id}
           onClick={() => setSelectedTag(tag.text)}
         >
           {tag.icon}
           <Text
             tag={selectedTag == tag.text ? 'body_bold_14' : 'body_regular_14'}
             color={selectedTag == tag.text ? 'white' : 'black'}
-            className=""
           >
             {tag.text}
           </Text>
