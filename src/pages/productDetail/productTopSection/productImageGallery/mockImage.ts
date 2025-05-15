cd8ee995dc68f58d3698a024ffa2d3cca69651f5
jeonghoon11 <jih361@naver.com>
2025-05-15 13:38:20 +0900

    [feat] mockdata 추가  (#50)

---
+++ src/pages/productDetail/productTopSection/productImageGallery/mockImage.ts
@@ -0,0 +1,5 @@
+import IcShield from '@/assets/svg/ic_shield_green.svg?react';
+import IcPageLeft from '@/assets/svg/ic_necessity_black.svg?react';
+import IcShare from '@/assets/svg/ic_share.svg?react';
+
+export const mockProductImages = [IcShield, IcPageLeft, IcShare];
---
+++ src/shared/types/product.ts
@@ -0,0 +1,12 @@
+export interface Product {
+  company: string;
+  productName: string;
+  discountRate: number;
+  originalPrice: number;
+  discountPrice: number;
+  productImages: string[];
+  productColors: string[];
+  productDetails: string[];
+  reviewCount: number;
+  productId: number;
+}
