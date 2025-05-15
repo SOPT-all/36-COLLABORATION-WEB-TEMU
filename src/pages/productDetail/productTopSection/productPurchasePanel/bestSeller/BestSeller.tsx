255d370f5e5eedc61a846ad1825e2832f817ec28
jeonghoon11 <jih361@naver.com>
2025-05-15 13:51:05 +0900

    [refactor] 사용하지 않는 import 삭제 (#50)

--- src/pages/productDetail/productTopSection/productPurchasePanel/bestSeller/BestSeller.tsx
+++ src/pages/productDetail/productTopSection/productPurchasePanel/bestSeller/BestSeller.tsx
@@ -1,5 +1,4 @@
 import * as styles from '@pages/productDetail/productTopSection/productPurchasePanel/bestSeller/BestSeller.css';
-import Text from '@shared/components/text/Text';
 
 const BestSeller = () => {
   return <div className={styles.bestSeller}>베스트셀러 상품</div>;
