6f1a74927e68ef5c92b9a9b1d795b58909ff6856
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-05-15 10:19:10 +0900

    [feat] Tag 컴포넌트에 Text 컴포넌트 적용 및 불필요힌 Tag.css 제거

--- src/pages/home/components/Tag.css.ts
+++ src/pages/home/components/Tag.css.ts
@@ -1,6 +1,5 @@
 import { vars } from '@styles/theme.css';
 import { style } from '@vanilla-extract/css';
-import { recipe } from '@vanilla-extract/recipes';
 
 export const tagWrapper = style({
   width: '108.6rem',
@@ -9,33 +8,21 @@ export const tagWrapper = style({
   flexWrap: 'nowrap',
 });
 
-export const tag = recipe({
-  base: {
-    display: 'flex',
-    flexDirection: 'column',
-    width: '9.4rem',
-    height: '9.2rem',
-    padding: '1.6rem',
-    justifyContent: 'center',
-    alignItems: 'center',
-    gap: '0.4rem',
-    flexShrink: 0,
-    borderRadius: '0.4rem',
-    background: vars.color.gray0,
-    color: vars.color.gray8,
-    ...vars.font.body_regular_14,
-    cursor: 'pointer',
-  },
-  variants: {
-    selected: {
-      true: {
-        background: vars.color.point_orange,
-        color: vars.color.white,
-        ...vars.font.body_bold_14,
-      },
-    },
-  },
-  defaultVariants: {
-    selected: false,
-  },
+export const tag = style({
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  gap: '0.4rem',
+  flexShrink: 0,
+
+  width: '9.4rem',
+  height: '9.2rem',
+  padding: '1.6rem',
+  borderRadius: '0.4rem',
+
+  background: vars.color.gray0,
+  color: vars.color.gray8,
+
+  cursor: 'pointer',
 });
--- src/pages/home/components/Tag.tsx
+++ src/pages/home/components/Tag.tsx
@@ -1,41 +1,46 @@
 import { useState } from 'react';
-import * as styles from './Tag.css.ts';
+import * as styles from '@/pages/home/components/Tag.css';
 
-import Ic_heart from '@svg/ic_recommend_black.svg?react';
-import Ic_book from '@svg/ic_book_black.svg?react';
-import Ic_car from '@svg/ic_car_black.svg?react';
-import Ic_clothes from '@svg/ic_goods_black.svg?react';
-import Ic_digital from '@svg/ic_digital_black.svg?react';
-import Ic_house from '@svg/ic_necessity_black.svg?react';
-import Ic_infant from '@svg/ic_baby_black.svg?react';
-import Ic_kitchen from '@svg/ic_kitchen_black.svg?react';
-import Ic_sport from '@svg/ic_sport_black.svg?react';
+import {
+  IcRecommendBlack,
+  IcBookBlack,
+  IcCarBlack,
+  IcGoodsBlack,
+  IcDigitalBlack,
+  IcNecessityBlack,
+  IcBabyBlack,
+  IcKitchenBlack,
+  IcSportBlack,
+} from '@svg/index.ts';
+import Text from '@shared/components/text/Text';
 
 const Tag = () => {
   const [selectedTag, setSelectedTag] = useState<string | null>(null);
 
   const TAG = [
-    { text: '인기', icon: <Ic_heart width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '도서', icon: <Ic_book width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '자동차용품', icon: <Ic_car width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '패션의류', icon: <Ic_clothes width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '디지털기기', icon: <Ic_digital width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '생활/주거', icon: <Ic_house width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '유아동', icon: <Ic_infant width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '주방용품', icon: <Ic_kitchen width={'3.2rem'} height={'3.2rem'} /> },
-    { text: '스포츠', icon: <Ic_sport width={'3.2rem'} height={'3.2rem'} /> },
+    { text: '인기', icon: <IcRecommendBlack width={'3.2rem'} height={'3.2rem'} /> },
+    { text: '도서', icon: <IcBookBlack width={'3.2rem'} height={'3.2rem'} /> },
+    { text: '자동차용품', icon: <IcCarBlack width={'3.2rem'} height={'3.2rem'} /> },
+    { text: '패션의류', icon: <IcGoodsBlack width={'3.2rem'} height={'3.2rem'} /> },
+    { text: '디지털기기', icon: <IcDigitalBlack width={'3.2rem'} height={'3.2rem'} /> },
+    { text: '생활/주거', icon: <IcNecessityBlack width={'3.2rem'} height={'3.2rem'} /> },
+    { text: '유아동', icon: <IcBabyBlack width={'3.2rem'} height={'3.2rem'} /> },
+    { text: '주방용품', icon: <IcKitchenBlack width={'3.2rem'} height={'3.2rem'} /> },
+    { text: '스포츠', icon: <IcSportBlack width={'3.2rem'} height={'3.2rem'} /> },
   ];
 
   return (
     <div className={styles.tagWrapper}>
       {TAG.map(tag => (
-        <div
-          key={tag.text}
-          className={styles.tag({ selected: selectedTag === tag.text })}
-          onClick={() => setSelectedTag(tag.text)}
-        >
+        <div key={tag.text} onClick={() => setSelectedTag(tag.text)}>
           {tag.icon}
-          <p>{tag.text}</p>
+          <Text
+            tag={selectedTag == tag.text ? 'body_bold_14' : 'body_regular_14'}
+            color={selectedTag == tag.text ? 'white' : 'black'}
+            className=""
+          >
+            {tag.text}
+          </Text>
         </div>
       ))}
     </div>
