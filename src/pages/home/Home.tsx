309a4b2538cf5a83c1d13724fd0954dd208063f6
hamxxn <150767569+hamxxn@users.noreply.github.com>
2025-05-15 18:09:30 +0900

    [code review] home 더미데이터 파일 분리 및 api 타입 생성(#36)

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,195 +1,13 @@
 import * as styles from '@/pages/home/Home.css';
-import { ImgMainBanner, IcFlashBlack, IcChevronForwardBlack, FamilyMonthTitle } from '@svg/index';
-
+import { IcFlashBlack, IcChevronForwardBlack } from '@svg/index';
+import ImgMainBanner from '@/../public/img/imgMainBanner.png';
+import FamilyMonthTitle from '@/../public/img/familyMonthTitle.png';
 import Tag from '@/pages/home/components/Tag';
 import Text from '@shared/components/text/Text';
 import Card from '@shared/components/card/Card';
+import { dummyCardsL, dummyCardsXL } from './mockHomeData';
 
 const Home = () => {
-  const dummyCardsL = [
-    {
-      size: 'l',
-      repurchase: true,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+1',
-      productName: '모던 디자인 테이블 램프',
-      discountRate: 20,
-      discountPrice: 49000,
-      reviewCount: 100,
-    },
-    {
-      size: 'l',
-      repurchase: false,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+2',
-      productName: '심플 미니멀 스탠드',
-      discountRate: 15,
-      discountPrice: 35000,
-      reviewCount: 42,
-    },
-    {
-      size: 'l',
-      repurchase: true,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+3',
-      productName: '북유럽 스타일 조명',
-      discountRate: 10,
-      discountPrice: 59000,
-      reviewCount: 87,
-    },
-    {
-      size: 'l',
-      repurchase: false,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+4',
-      productName: 'LED 데스크 라이트',
-      discountRate: 25,
-      discountPrice: 41000,
-      reviewCount: 130,
-    },
-    {
-      size: 'l',
-      repurchase: false,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+4',
-      productName: 'LED 데스크 라이트',
-      discountRate: 25,
-      discountPrice: 41000,
-      reviewCount: 130,
-    },
-  ] as const;
-  const dummyCardsXL = [
-    {
-      size: 'xl',
-      repurchase: true,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+1',
-      productName: '모던 디자인 테이블 램프',
-      discountRate: 20,
-      discountPrice: 49000,
-      reviewCount: 100,
-    },
-    {
-      size: 'xl',
-      repurchase: false,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+2',
-      productName: '심플 미니멀 스탠드',
-      discountRate: 15,
-      discountPrice: 35000,
-      reviewCount: 42,
-    },
-    {
-      size: 'xl',
-      repurchase: true,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+3',
-      productName: '북유럽 스타일 조명',
-      discountRate: 10,
-      discountPrice: 59000,
-      reviewCount: 87,
-    },
-    {
-      size: 'xl',
-      repurchase: false,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+4',
-      productName: 'LED 데스크 라이트',
-      discountRate: 25,
-      discountPrice: 41000,
-      reviewCount: 130,
-    },
-    {
-      size: 'xl',
-      repurchase: false,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+4',
-      productName: 'LED 데스크 라이트',
-      discountRate: 25,
-      discountPrice: 41000,
-      reviewCount: 130,
-    },
-    {
-      size: 'xl',
-      repurchase: true,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+1',
-      productName: '모던 디자인 테이블 램프',
-      discountRate: 20,
-      discountPrice: 49000,
-      reviewCount: 100,
-    },
-    {
-      size: 'xl',
-      repurchase: false,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+2',
-      productName: '심플 미니멀 스탠드',
-      discountRate: 15,
-      discountPrice: 35000,
-      reviewCount: 42,
-    },
-    {
-      size: 'xl',
-      repurchase: true,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+3',
-      productName: '북유럽 스타일 조명',
-      discountRate: 10,
-      discountPrice: 59000,
-      reviewCount: 87,
-    },
-    {
-      size: 'xl',
-      repurchase: false,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+4',
-      productName: 'LED 데스크 라이트',
-      discountRate: 25,
-      discountPrice: 41000,
-      reviewCount: 130,
-    },
-    {
-      size: 'xl',
-      repurchase: false,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+4',
-      productName: 'LED 데스크 라이트',
-      discountRate: 25,
-      discountPrice: 41000,
-      reviewCount: 130,
-    },
-    {
-      size: 'xl',
-      repurchase: true,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+1',
-      productName: '모던 디자인 테이블 램프',
-      discountRate: 20,
-      discountPrice: 49000,
-      reviewCount: 100,
-    },
-    {
-      size: 'xl',
-      repurchase: false,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+2',
-      productName: '심플 미니멀 스탠드',
-      discountRate: 15,
-      discountPrice: 35000,
-      reviewCount: 42,
-    },
-    {
-      size: 'xl',
-      repurchase: true,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+3',
-      productName: '북유럽 스타일 조명',
-      discountRate: 10,
-      discountPrice: 59000,
-      reviewCount: 87,
-    },
-    {
-      size: 'xl',
-      repurchase: false,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+4',
-      productName: 'LED 데스크 라이트',
-      discountRate: 25,
-      discountPrice: 41000,
-      reviewCount: 130,
-    },
-    {
-      size: 'xl',
-      repurchase: false,
-      imageUrl: 'https://via.placeholder.com/350x150.png?text=Product+4',
-      productName: 'LED 데스크 라이트',
-      discountRate: 25,
-      discountPrice: 41000,
-      reviewCount: 130,
-    },
-  ] as const;
   return (
     <>
       {
@@ -200,12 +18,12 @@ const Home = () => {
           <div className={styles.forwardTitleWrapper}>
             <div className={styles.forwardTitle}>
               <IcFlashBlack width={'2.4rem'} height={'2.4rem'} />
-              <Text tag="head_bold_24" color="black" className="">
+              <Text tag="head_bold_24" color="black">
                 번개특가
               </Text>
               <IcChevronForwardBlack width={'3.2rem'} height={'3.2rem'} />
             </div>
-            <Text tag="body_regular_16" color="gray4" className="">
+            <Text tag="body_regular_16" color="gray4">
               서둘러 주세요! 혜택가로 인기 상품을 놓치지 말고 구매하세요
             </Text>
           </div>
@@ -216,12 +34,12 @@ const Home = () => {
           </div>
         </section>
         <section className={styles.sectionStyle}>
-          <ImgMainBanner width={'108.6rem'} height={'22.4rem'} />
+          <img src={ImgMainBanner} className={styles.imgMainBanner} />
         </section>
         <section className={styles.sectionStyle}>
           <div className={styles.listTitleWrapper}>
-            <FamilyMonthTitle width="11.5rem" height="2rem" />
-            <Text tag="head_bold_24" color="black" className="">
+            <img src={FamilyMonthTitle} className={styles.familyMonthTitle} />
+            <Text tag="head_bold_24" color="black">
               관심 상품 둘러보기
             </Text>
           </div>
