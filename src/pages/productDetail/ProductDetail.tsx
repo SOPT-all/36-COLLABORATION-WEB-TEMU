8dc7194c0275a68235b22d9e171b61cd804b64c7
jeonghoon11 <jih361@naver.com>
2025-05-15 14:21:51 +0900

    [fix] 임시로 eslint 피하기 추후 꼭 삭제하기(#50)

--- src/pages/productDetail/ProductDetail.tsx
+++ src/pages/productDetail/ProductDetail.tsx
@@ -2,6 +2,8 @@ import ProductTopSection from './productTopSection/ProductTopSection';
 import { data } from '@pages/productDetail/mockData';
 
 const ProductDetail = () => {
+  // build 에러 때문에 임시로 처리했습니다. 나중에 꼭 없앨게요
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const {
     productDetails: _productDetails,
     reviewCount: _reviewCount,
@@ -12,6 +14,7 @@ const ProductDetail = () => {
   return (
     <div>
       {/* 아직 값을 다 쓰지 않아 에러가 발생하여 잠시 any타입으로 지정하였습니다. 추후 삭제예정 */}
+      {/* eslint-disable-next-line @typescript-eslint/no-explicit-any */}
       <ProductTopSection restData={restData as any} />
     </div>
   );
