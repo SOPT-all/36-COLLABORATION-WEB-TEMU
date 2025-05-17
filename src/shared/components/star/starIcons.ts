163259c56b4074f8513ddcbf8bdd2b4b12c705e3
jeonghoon11 <jih361@naver.com>
2025-05-17 15:33:57 +0900

    [code review] import위치 수정(#53)

--- src/shared/components/star/starIcons.ts
+++ src/shared/components/star/starIcons.ts
@@ -1,24 +1,26 @@
 import type { ComponentType, SVGProps } from 'react';
-import IcStar1 from '@svg/ic_star_1_sm.svg?react';
-import IcStar1_5 from '@svg/ic_star_1_5_sm.svg?react';
-import IcStar2 from '@svg/ic_star_2_sm.svg?react';
-import IcStar2_5 from '@svg/ic_star_2_5_sm.svg?react';
-import IcStar3 from '@svg/ic_star_3_sm.svg?react';
-import IcStar3_5 from '@svg/ic_star_3_5_sm.svg?react';
-import IcStar4 from '@svg/ic_star_4_sm.svg?react';
-import IcStar4_5 from '@svg/ic_star_4_5_sm.svg?react';
-import IcStar5 from '@svg/ic_star_5_sm.svg?react';
+import {
+  IcStar1Sm,
+  IcStar15Sm,
+  IcStar2Sm,
+  IcStar25Sm,
+  IcStar3Sm,
+  IcStar35Sm,
+  IcStar4Sm,
+  IcStar45Sm,
+  IcStar5Sm,
+} from '@svg/index';
 
 export const starIconMap: Record<number, ComponentType<SVGProps<SVGSVGElement>>> = {
-  1: IcStar1,
-  1.5: IcStar1_5,
-  2: IcStar2,
-  2.5: IcStar2_5,
-  3: IcStar3,
-  3.5: IcStar3_5,
-  4: IcStar4,
-  4.5: IcStar4_5,
-  5: IcStar5,
+  1: IcStar1Sm,
+  1.5: IcStar15Sm,
+  2: IcStar2Sm,
+  2.5: IcStar25Sm,
+  3: IcStar3Sm,
+  3.5: IcStar35Sm,
+  4: IcStar4Sm,
+  4.5: IcStar45Sm,
+  5: IcStar5Sm,
 };
 
 export type StarScore = keyof typeof starIconMap;
