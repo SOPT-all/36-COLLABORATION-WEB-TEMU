e1e9d97c903e99b9cc2c676b47b66b196158245f
1jiwoo27 <chloey1227@naver.com>
2025-05-17 15:34:57 +0900

    [code review] css 파일 수정 (#55)

--- src/pages/search/Search.css.ts
+++ src/pages/search/Search.css.ts
@@ -6,17 +6,17 @@ export const container = style ({
   gap: '3.2rem',
 
   width: '108.8rem',
-  margin: '0 auto',
+  margin: '3.2rem auto 0 auto',
 });
 
 export const listWrapper = style({
   display: 'grid',
   gridTemplateColumns: 'repeat(auto-fit, minmax(30rem, 1fr))',
-  gap: '7.9rem 1.9rem',
+  gap: '3.6rem 1.9rem',
 });
 
 export const buttonWrapper = style({
-  margin: '8rem 0 18.8rem 0',
+  padding: '8rem 0 18.8rem 0',
 
   textAlign: 'center',
 })
--- src/pages/search/components/filterButton/FilterButton.css.ts
+++ src/pages/search/components/filterButton/FilterButton.css.ts
@@ -2,8 +2,5 @@ import { style } from '@vanilla-extract/css';
 
 export const container = style({
   display: 'flex',
-  flexWrap: 'wrap',
   gap: '1.2rem',
-
-  margin: '3.2rem 0 0 0',
 });
