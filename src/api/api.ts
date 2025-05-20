7f8b1434dcfcc29b56a4e687d0b6cba2095eb505
jeonghoon11 <jih361@naver.com>
2025-05-21 02:02:55 +0900

    [feat] 리뷰 api 연결(#71)

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,10 @@
 import { END_URL } from '@/constant/url';
 import { get } from '@api/instance';
 import type { ProductDetailResponseTypes } from '@pages/productDetail/types/response';
+import type { ProductReviewDetailResponseTypes } from '@pages/productDetail/productReviewSection/types/reviews';
 
 export const getProductDetail = (productId: number) =>
   get<ProductDetailResponseTypes>(END_URL.GET_PRODUCTS_DETAIL(productId));
+
+export const getProductDetailReviewSection = (productId: number) =>
+  get<ProductReviewDetailResponseTypes>(END_URL.GET_PRODUCTS_DETAIL_REVIEWS(productId));
--- src/api/queries.ts
+++ src/api/queries.ts
@@ -1,10 +1,17 @@
 import { useQuery } from '@tanstack/react-query';
-import { getProductDetail } from '@api/api';
+import { getProductDetail, getProductDetailReviewSection } from '@api/api';
 import { QUERY_KEYS } from '@/constant/queryKey';
 
 export const useGetProductDetail = (productId: number) => {
   return useQuery({
     queryKey: [QUERY_KEYS.PRODUCTS_DETAIL, productId],
     queryFn: () => getProductDetail(productId),
+  });
+};
+
+export const useGetProductDetailReview = (productId: number) => {
+  return useQuery({
+    queryKey: [QUERY_KEYS.PRODUCTS_DETAIL_REVIEWS, productId],
+    queryFn: () => getProductDetailReviewSection(productId),
   });
 };
