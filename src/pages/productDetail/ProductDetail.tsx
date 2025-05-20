d00977d52e8fb27db385c78a8a7c4d0c5d0844b1
jeonghoon11 <jih361@naver.com>
2025-05-21 02:33:47 +0900

    [feat] 제품 상세 api 연동(#73)

--- src/pages/productDetail/ProductDetail.tsx
+++ src/pages/productDetail/ProductDetail.tsx
@@ -1,14 +1,32 @@
 import ProductTopSection from '@pages/productDetail/productTopSection/ProductTopSection';
 import ProductReviewSection from '@pages/productDetail/productReviewSection/ProductReviewSection';
-import { mockDetailData } from '@pages/productDetail/productTopSection/constants/mockDetailData';
+// import { mockDetailData } from '@pages/productDetail/productTopSection/constants/mockDetailData';
+
+import { useParams } from 'react-router-dom';
+import { useGetProductDetail } from '@api/queries';
 
 const ProductDetail = () => {
+  // const {
+  //   // productDetails: productDetails,
+  //   // reviewCount: reviewCount,
+  //   // productId: productId,
+  //   ...restData
+  // } = mockDetailData;
+
+  const { id } = useParams<{ id: string }>();
+  const productId = Number(id);
+
+  const { data, isLoading, isError } = useGetProductDetail(productId);
+
+  if (isLoading) return <div>로딩 중...</div>;
+  if (isError) return <div>상품 정보를 불러오는 중 에러가 발생했습니다.</div>;
+
   const {
     // productDetails: productDetails,
     // reviewCount: reviewCount,
     // productId: productId,
     ...restData
-  } = mockDetailData;
+  } = data!; //필요한 필드만 빼고 전달 (희선님 코드 merge되면 수정)
 
   return (
     <>
